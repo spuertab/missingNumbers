f4490f50e52a7c3f6fa249079e243db17554ac74
Santiago Puerta Bernal <spb1915@hotmail.com>
2019-06-21 19:31:49 -0500

    primer commit, solucion NET Core Console con proyecto de pruebas unitarias

---
+++ src/PuntosColombia.MissingNumbers.Test/MissingNumbersTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PuntosColombia.MissingNumbers.Test
+{
+    [TestClass]
+    public class MissingNumbersTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/PuntosColombia.MissingNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PuntosColombia.MissingNumbers
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ARREGLO UNO INGRESAR 203 204 205 206 207 208 203 204 205 206
+            // ARREGLO DOS INGRESAR 203 204 204 205 206 207 205 208 203 206 205 206 204
+            // RESULTADO 204 205 206
+
+            Console.WriteLine("Enter numbers list one");
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            Console.WriteLine("Enter numbers list two");
+            int[] brr = Array.ConvertAll(Console.ReadLine().Split(' '), brrTemp => Convert.ToInt32(brrTemp));
+
+            Console.WriteLine("Missing numbers");
+            MissingNumbers missingNumbers = new MissingNumbers(arr, brr);
+            int[] result = missingNumbers.GetMissingNumbers();
+
+            Console.WriteLine(result.Count() > 0 ? string.Join(" ", result) : "none");
+            Console.ReadLine();
+        }
+    }
+}
